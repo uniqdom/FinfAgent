92246204938864f1c02a4bb33be5d11dfbeffd89
uniqdom <slashytu@hotmail.com>
2012-12-17 16:38:23 -0300

    added ./src

---
+++ src/com/flurry/android/FlurryAgent.java
@@ -0,0 +1,26 @@
+package com.flurry.android;
+
+import android.content.Context;
+import java.util.Map;
+
+
+
+public class FlurryAgent{
+	
+	public static void onStartSession(android.content.Context cntxt, String apiKey){
+        
+    	}
+	
+	public static void onEndSession(android.content.Context cntxt){
+        
+    	}
+	
+	public static void onEvent(String string) {
+ 
+    }
+	
+	public static void onEvent(String string, Map<String, String> map) { 
+		
+	}
+
+}
